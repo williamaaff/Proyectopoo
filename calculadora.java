d2ae701e2e987544a2f7bd702d19f9a866f2c4ad
cclb11 <80927931+cclb11@users.noreply.github.com>
2021-04-17 21:24:30 -0500

    Crear ventana

--- calculadora.java
+++ calculadora.java
@@ -1,7 +1,13 @@
-
+import javax.swing.JFrame;
 
 public class calculadora {
-  public static void main(String[] args) {
-        // 
-    }
+
+  public void ventana(){
+
+    JFrame ventana=new JFrame("Calculadora");
+    ventana.setVisible(true);
+    ventana.setSize(300, 150);
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
+
+}
