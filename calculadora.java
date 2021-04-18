d460da4db10492c988605711ec89791ccd0d153c
cclb11 <80927931+cclb11@users.noreply.github.com>
2021-04-18 10:52:04 -0500

    Color y títulos de la ventana
    Se hacen los ajustes del tamaño de ventana, titulo, color.

--- calculadora.java
+++ calculadora.java
@@ -1,13 +1,24 @@
-import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.*;
 
 public class calculadora {
 
-  public void ventana(){
+  public void ventanabasica(){//Constructor
 
-    JFrame ventana=new JFrame("Calculadora");
-    ventana.setVisible(true);
-    ventana.setSize(300, 150);
-    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JFrame ventana=new JFrame("Calculadora");//CARACTERISTICAS DE LA VENTNA Y TITULO
+    ventana.setVisible(true);//Hacer visible la ventana
+    ventana.setSize(300, 400);//Dimensiones de la ventana
+    ventana.setLocationRelativeTo(null);//Posicion central de la ventana
+    ventana.setResizable(false);//Hacer la ventana ajustable o no
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Cerrar la ventana al dar en "X"
+
+    JPanel panel=new JPanel();
+    panel.setBackground(Color.BLUE);//Configuracion de Color de panel
+    ventana.getContentPane().add(panel);//Añadir panel a la ventana
+
+    JLabel etiquetas=new JLabel("Calculadora Basica");//Titulo del programa
+    panel.add(etiquetas);
   }
 
 }
+
